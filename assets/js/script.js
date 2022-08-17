505e2d824df55a5ed3d483919658ee31e872e37a
Gilmar Alves <gilmaralves914@gmail.com>
2022-08-16 23:59:39 -0300

    Adicionando arquivos já prontos

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours()
+    let minutos = date.getMinutes();
+    let segundos = date.getSeconds();
+    let periodo = '☽';
+
+   
+    if (hrs < 12) {
+        hrs = hrs - 12;
+        periodo = '☀';
+    }
+   
+    hrs = hrs < 10 ? "0" + hrs : hrs;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+   
+    document.querySelector('.time').innerHTML = `${hrs}:${minutos}:${segundos}  ${periodo}`;
+
+    setTimeout(clock, 1000);
+};
+clock();
